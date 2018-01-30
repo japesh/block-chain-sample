f949c11761704ea3e958cec3031f3dd801f20e1f
japesh <japesh@daffodilsw.com>
2018-01-30 20:32:03 +0530

    adding proof of work

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
-import {SHA256} from "crypto-js"
+import { SHA256 } from "crypto-js";
 class Block {
-    constructor(index, timestamp, data, previousHash){
-        this.index=index;
-        this.timestamp=timestamp;
-        this.data=data;
-        this.previousHash=previousHash;
-        this.hash = this.calculateHash()
-    }
-    calculateHash(){
-        return SHA256(this.index+this.previousHash+this.timestamp+JSON.stringify(this.data)).toString();
-    }
-}
-class BlockChain{
-    constructor(){
-        this.chain=[this.createGenesisBlock()]
-    }
+  constructor(index, timestamp, data, previousHash, difficulty) {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.nonce = 0;
+    this.hash = this.mineBlock(difficulty);
+  }
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+  mineBlock(difficulty) {
+    let hash = this.calculateHash();
+    while (hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
 
-    /*
+      hash = this.calculateHash();
+    }
+    return hash;
+  }
+}
+class BlockChain {
+  constructor(difficulty = 4) {
+    this.difficulty = difficulty;
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  /*
      * Gensis block is the first block in the chain and it don't have previous hash value
      *
      * */
-    createGenesisBlock(){
-        return new Block(0,new Date(), "Genesis Block", "");
+  createGenesisBlock() {
+    return new Block(0, new Date(), "Genesis Block", "", 0);
+  }
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+  addBlock(data) {
+    let index = this.chain.length;
+    this.chain.push(new Block(index, new Date(), data, this.getLatestBlock().hash, this.difficulty));
+  }
+  isChainValid() {
+    let chain = this.chain;
+    let length = this.chain.length;
+    for (let i = 1; i < length; i++) {
+      let block = chain[i];
+      if (block.calculateHash() !== block.hash || block.previousHash !== chain[i - 1].hash) {
+        return false;
+      }
     }
-    getLatestBlock(){
-        return this.chain[this.chain.length-1];
-    }
-    addBlock(data){
-        let index = this.chain.length
-        this.chain.push(new Block(index, new Date(),data, this.getLatestBlock().hash))
-    }
-    isChainValid(){
-        let chain = this.chain;
-        let length =  this.chain.length;
-        for(let i =1; i< length; i++){
-            let block = chain[i]
-            if(block.calculateHash()!== block.hash||block.previousHash!==chain[i-1].hash){
-                return false
-            }
-        }
-        return true
-    }
+    return true;
+  }
 }
 /*
 * tempring of block chain sample
@@ -50,13 +61,15 @@ class BlockChain{
 * temper any kind of block
 * */
 const japeshCoin = new BlockChain();
-japeshCoin.addBlock({amount:4})
-japeshCoin.addBlock({amount:10})
-console.log(japeshCoin.isChainValid())
-japeshCoin.chain[1].data.amount=100
+japeshCoin.addBlock({ amount: 4 });
+console.log("block 1 is mined>>>>");
+japeshCoin.addBlock({ amount: 10 });
+console.log("block 2 is mined>>>>");
+console.log(japeshCoin.isChainValid());
+japeshCoin.chain[1].data.amount = 100;
 japeshCoin.chain[1].hash = japeshCoin.chain[1].calculateHash();
-console.log(japeshCoin.isChainValid())
-japeshCoin.chain[2].previousHash = japeshCoin.chain[1].hash
+console.log(japeshCoin.isChainValid());
+japeshCoin.chain[2].previousHash = japeshCoin.chain[1].hash;
 japeshCoin.chain[2].hash = japeshCoin.chain[2].calculateHash();
-console.log(japeshCoin.isChainValid())
+console.log(japeshCoin.isChainValid());
 // console.log(JSON.stringify(japeshCoin));
